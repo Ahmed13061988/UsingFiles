093b88de1ea70aa9fed5ce81975c610995b8f14d
Ahmed Hussein <wolktheline@yahoo.com>
2023-09-08 19:13:12 -0600

    creating new file

--- main.py
+++ main.py
@@ -3,5 +3,8 @@
 #     print(content)
 
 
-with open("my_file.txt", mode="w") as file:
+with open("my_file.txt", mode="a") as file:
     file.write("New Text")
+
+with open("new_file.txt", mode="w") as new_file:
+    new_file.write("Hello, my name is Ramy")
