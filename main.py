4eeac5fd328252bc6eaab09fa5f78652e8100a7a
Ahmed Hussein <wolktheline@yahoo.com>
2023-09-10 16:32:14 -0600

    Getting a file from another path

--- main.py
+++ main.py
@@ -3,8 +3,8 @@
 #     print(content)
 
 
-with open("my_file.txt", mode="a") as file:
-    file.write("New Text")
+with open("/Users/ahmedhussein/Desktop/new_file1.txt") as file:
+    content = file.read()
+    print(content)
 
-with open("new_file.txt", mode="w") as new_file:
-    new_file.write("Hello, my name is Ramy")
+
